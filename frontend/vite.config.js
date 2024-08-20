0e8cd2904c08310e261ad44af2df6e4d6ad15a64
Sk Nadim <128034110+Sknadim6297@users.noreply.github.com>
2024-08-21 00:00:40 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    proxy: {
-      '/api': {
-        target: 'https://mern-real-time-chat-app-3.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
