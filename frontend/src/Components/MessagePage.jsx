565970da3aef0b1355dab663cf695f23ec5a2c3e
SK NADIM <128034110+Sknadim6297@users.noreply.github.com>
2024-08-24 22:03:15 +0530

    hello3

--- frontend/src/Components/MessagePage.jsx
+++ frontend/src/Components/MessagePage.jsx
@@ -109,15 +109,15 @@ const MessagePage = () => {
   return (
     <div
       style={{ backgroundImage: `url(${backgroundImage})` }}
-      className='bg-no-repeat bg-cover dark:bg-gray-900 dark:text-white h-[90vh] flex flex-col'>
-      
+      className='bg-no-repeat bg-cover dark:bg-gray-900 dark:text-white h-[calc(100vh-3.5rem)] flex flex-col'>
+
       <header className='sticky top-0 h-14 bg-white dark:bg-gray-800 flex justify-between items-center px-4 border-b border-zinc-900'>
         <div className='flex items-center gap-4'>
           <Link to="/" className='lg:hidden'>
             <FaAngleLeft size={25} />
           </Link>
           <Avatar
-            className='Overflow-hidden rounded-full'
+            className='overflow-hidden rounded-full'
             imageUrl={dataUser.profile_pic}
             name={dataUser.name}
             userId={dataUser._id}
@@ -136,11 +136,11 @@ const MessagePage = () => {
       </header>
 
       <main className='flex-1 overflow-hidden'>
-        <section className='h-[calc(100vh-120px)] overflow-x-hidden overflow-y-scroll scrollbar relative bg-slate-200 bg-opacity-50 dark:bg-gray-800'>
+        <section className='h-[calc(100vh-120px)] overflow-y-auto scrollbar bg-slate-200 bg-opacity-50 dark:bg-gray-800'>
           <div className='flex flex-col gap-2 py-2 mx-2' ref={currentMessage}>
             {allMessage.map((msg, index) => (
               <div
-                className={`p-1 py-1 rounded w-fit max-w-[280px] md:max-w-sm lg:max-w-md ${user._id === msg.msgByUserId ? "ml-auto bg-teal-100 dark:bg-teal-700" : "bg-white dark:bg-gray-700"}`}
+                className={`p-1 py-1 rounded w-fit max-w-[80%] md:max-w-sm lg:max-w-md ${user._id === msg.msgByUserId ? "ml-auto bg-teal-100 dark:bg-teal-700" : "bg-white dark:bg-gray-700"}`}
                 key={index}
               >
                 <div className='w-full relative'>
@@ -159,7 +159,7 @@ const MessagePage = () => {
                 <IoClose size={30} />
               </div>
               <div className='bg-white p-3 dark:bg-gray-800'>
-                <img src={message.imageUrl} alt='uploadImage' className='aspect-square w-full h-full max-w-sm m-2 object-scale-down' />
+                <img src={message.imageUrl} alt='uploadImage' className='w-full h-full max-w-sm m-2 object-scale-down' />
               </div>
             </div>
           )}
@@ -170,7 +170,7 @@ const MessagePage = () => {
                 <IoClose size={30} />
               </div>
               <div className='bg-white p-3 dark:bg-gray-800'>
-                <video src={message.videoUrl} className='aspect-square w-full h-full max-w-sm m-2 object-scale-down' controls muted autoPlay />
+                <video src={message.videoUrl} className='w-full h-full max-w-sm m-2 object-scale-down' controls muted autoPlay />
               </div>
             </div>
           )}
@@ -183,10 +183,10 @@ const MessagePage = () => {
         </section>
       </main>
 
-      <section className='h-16  bg-white dark:bg-gray-800 flex items-center  border-t border-zinc-900'>
+      <section className='min-h-[4rem] bg-white dark:bg-gray-800 flex items-center border-t border-zinc-900'>
         <div className='relative'>
-          <button onClick={handleUploadImageVideoOpen} className='flex justify-center items-center w-11 h-11 rounded-full hover:bg-red-400 hover:text-white'>
-            <FaPlus size={20} />
+          <button onClick={handleUploadImageVideoOpen} className='flex justify-center items-center w-12 h-11 rounded-full hover:bg-red-400 hover:text-white'>
+            <FaPlus size={18} />
           </button>
 
           {openImageVideoUpload && (
@@ -218,22 +218,22 @@ const MessagePage = () => {
           )}
         </div>
 
-        <form className='h-full w-full flex gap-2' onSubmit={handleSendMessage}>
+        <form className='flex-1 flex gap-2' onSubmit={handleSendMessage}>
           <input
             type="text"
             name='text'
             placeholder="Message..."
-            className=' flex-1 rounded-full px-5 my-2  border-0 focus:ring-0 focus:outline-none bg-slate-200 dark:bg-gray-700'
+            className='flex-1 rounded-full px-5 bg-slate-200 dark:bg-gray-700'
             value={message.text}
             onChange={handleOnChange}
           />
-          <button type='submit' className=' w-16 flex justify-center items-center rounded-full hover:bg-red-400 hover:text-white'>
-            <IoSend size={18} />
+          <button type="submit" className='w-11 h-11 flex justify-center items-center rounded-full hover:bg-red-400 hover:text-white'>
+            <IoSend size={20} />
           </button>
         </form>
       </section>
     </div>
   );
-};
+}
 
 export default MessagePage;
